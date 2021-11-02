7bbe047e3e5366c9fd6e7d536654891f6d7512f1
BimalNSU <bimal.nsu.cse@gmail.com>
2021-11-03 02:09:55 +0600

    added navbar menus

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -69,7 +69,32 @@
       
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
-
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i>
+            <span>Sales</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="{{url('/')}}"><i class="fa fa-circle-o"></i> Sales Create</a></li>
+            <li><a href="{{url('sales/')}}"><i class="fa fa-circle-o"></i> Sales List</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i>
+            <span>Products</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="{{url('/')}}"><i class="fa fa-circle-o"></i> Product Add</a></li>
+            <li><a href="{{url('sales/')}}"><i class="fa fa-circle-o"></i> Product List</a></li>
+          </ul>
+        </li>
       </ul>
     </section>
     <!-- /.sidebar -->
