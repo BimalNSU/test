cf9d0938f4c85174cc6972c990d321206bd5ce64
BimalNSU <bimal.nsu.cse@gmail.com>
2021-11-02 11:25:57 +0600

    define all api endpoints

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/','SalesController@index');
+Route::get('sales/','SalesController@getSalesList');
+Route::get('sales/{sales_id}','SalesController@getSalesDetails')->name('salesDetails');
+Route::get('sales/{sales_id}/edit','SalesController@editPage')->name('salesEditPage');;
+Route::POST('sales/','SalesController@create')->name('createSales');
+Route::PUT('sales/{sales_id}','SalesController@update')->name('salesUpdate');
+Route::DELETE('sales/{sales_id}','SalesController@delete')->name('salesDelete');
+
+Route::POST('products/','ProductController@create')->name('createProduct');
+Route::get('products/','ProductController@getProductList');
+
+Route::get('products/create','ProductController@createPage');
+Route::get('products/{product_id}/edit','ProductController@editPage');
+
+Route::get('products/{product_id}','ProductController@details')->name('product_details');
+
+Route::PUT('products/{product_id}','ProductController@update')->name('productUpdate');
+Route::DELETE('products/{product_id}','ProductController@delete');
