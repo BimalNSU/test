814e9a2dc667f06182e163ffc0ac8c0017da6bfc
BimalNSU <bimal.nsu.cse@gmail.com>
2021-11-03 20:36:25 +0600

    add & modified api endpoints

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,18 @@
 
 Route::get('/','SalesController@index');
 Route::get('sales/','SalesController@getSalesList');
-Route::get('sales/{sales_id}','SalesController@getSalesView')->name('salesView');
+// Route::get('sales/{sales_id}','SalesController@details');
+Route::get('sales/{sales_id}/view','SalesController@getSalesView')->name('salesView');
 Route::get('sales/{sales_id}/edit','SalesController@getSalesEdit')->name('salesEdit');
 Route::POST('sales/','SalesController@create')->name('createSales');
 Route::PUT('sales/{sales_id}','SalesController@edit')->name('salesUpdate');
 Route::DELETE('sales/{sales_id}','SalesController@delete')->name('salesDelete');
 
-Route::POST('products/','ProductController@create')->name('createProduct');
-Route::get('products/','ProductController@getProductList');
-
-Route::get('products/create','ProductController@createPage');
-Route::get('products/{product_id}/edit','ProductController@editPage');
-
-Route::get('products/{product_id}','ProductController@details')->name('product_details');
-
-Route::PUT('products/{product_id}','ProductController@update')->name('productUpdate');
-Route::DELETE('products/{product_id}','ProductController@delete');
+Route::POST('products/','ProductController@create');             // rest api
+Route::POST('products/create','ProductController@createPage');  // web api
+Route::get('products/','ProductController@getProductList');     // web & rest api
+Route::get('product/{product_id}','ProductController@details')->name('product_details');     // rest api
+Route::get('products/{product_id}/view','ProductController@details')->name('productView');   //web api
+Route::get('products/{product_id}/edit','ProductController@editPage')->name('productEdit');  //web api
+Route::PUT('products/{product_id}','ProductController@update')->name('productUpdate');      // rest api
+Route::DELETE('products/{product_id}','ProductController@delete')->name('productDelete');   // rest api
